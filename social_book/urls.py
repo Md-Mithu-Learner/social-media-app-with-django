60d058f8bdec5d72758d21188e90765a34763de4
Mithu <mdmithu.learner@gmail.com>
2023-05-29 03:46:59 +0600

    Add Account Profile settings

--- social_book/urls.py
+++ social_book/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from social_book.views import index, signup
+from social_book.views import index, signup, signin, logout, settings
 
 app_name = 'social_book'
 
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', signup, name='signup'),
+    path('signin/', signin, name='signin'),
+    path('logout/', logout, name='logout'),
+    path('settings/', settings, name='settings')
 ]
--- social_book/views.py
+++ social_book/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from social_book.models import Profile
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
+def settings(request,*args,**kwargs):
+    user_profile = Profile.objects.filter(user=request.user)
+    if request.method == 'POST':
+        if request.FILES.get('image')==None:
+            image = user_profile.profileimg
+            bio= request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg=image
+            user_profile.bio=bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image')!=None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('social_book:settings')
+
+    return render(request, 'social_book/setting.html', {'user_profile': user_profile})
+
+
 def index(request, *args, **kwargs):
     return render(request, 'social_book/index.html', )
 
@@ -26,12 +54,35 @@ def signup(request, *args, **kwargs):
             else:
                 user=User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+                #log user and redirect to settings.py
+                user_login=auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('social_book:signup')
+                return redirect('social_book:settings')
 
 
     else:
         messages.info(request, 'password not matching')
         return render(request, 'social_book/signup.html')
+def signin(request, *args, **kwargs):
+     if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('social_book:index')
+        else:
+            messages.info('credential invalid')
+            return redirect('social_book:signin')
+     else:
+         return render(request,'social_book/signin.html')
+def logout(request, *args, **kwargs):
+     auth.logout(request)
+     return redirect('social_book:signin')
+
+
+
